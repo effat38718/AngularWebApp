2085682c0b9d7d17d715218a51bba9410e4bf520
Effat Jahan <75140801+effat38718@users.noreply.github.com>
2021-07-12 03:33:46 +0600

    Added some fancy icons in the non functional toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  BASEURL: string = 'http://127.0.0.1:5000/'
   title = 'Angular7';
+
+  OnClickMenuIcon() {
+    console.log("MENU CLIKED");
+  }
+
+  OnClickAddIcon() {
+    console.log("ADD CLIKED");
+  }
+
+  OnClickCartIcon() {
+    console.log("JHURI CLIKED");
+  }
+
 }
